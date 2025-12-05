8ee33fc5cc1d86382114f5f7cb5e443399f735b7
KRXDevelops-Kaan <kaanuyumaz.developer@gmail.com>
2025-12-04 22:12:30 -0200

    Further clean-up

--- src/main/java/com/krxdevelops/artifexmagiae/ArtifexMagiaeClient.java
+++ src/main/java/com/krxdevelops/artifexmagiae/ArtifexMagiaeClient.java
@@ -1,6 +1,5 @@
 package com.krxdevelops.artifexmagiae;
 
-import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
@@ -10,22 +9,13 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-// This class will not load on dedicated servers. Accessing client side code from here is safe.
 @Mod(value = ArtifexMagiae.MODID, dist = Dist.CLIENT)
-// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
 @EventBusSubscriber(modid = ArtifexMagiae.MODID, value = Dist.CLIENT)
 public class ArtifexMagiaeClient {
     public ArtifexMagiaeClient(ModContainer container) {
-        // Allows NeoForge to create a config screen for this mod's configs.
-        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
-        // Do not forget to add translations for your config options to the en_us.json file.
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 
     @SubscribeEvent
-    static void onClientSetup(FMLClientSetupEvent event) {
-        // Some client setup code
-        ArtifexMagiae.LOGGER.info("HELLO FROM CLIENT SETUP");
-        ArtifexMagiae.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-    }
+    static void onClientSetup(FMLClientSetupEvent event) {}
 }
